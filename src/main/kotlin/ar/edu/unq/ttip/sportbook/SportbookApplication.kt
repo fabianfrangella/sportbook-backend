cda84c3266512a7872a9085bac6296ff67f9f65d
Fabian Frangella <fabianfrangella@gmail.com>
2025-08-17 19:44:44 -0300

    initial-commit

---
+++ src/main/kotlin/ar/edu/unq/ttip/sportbook/SportbookApplication.kt
@@ -0,0 +1,11 @@
+package ar.edu.unq.ttip.sportbook
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SportbookApplication
+
+fun main(args: Array<String>) {
+	runApplication<SportbookApplication>(*args)
+}
---
+++ src/test/kotlin/ar/edu/unq/ttip/sportbook/SportbookApplicationTests.kt
@@ -0,0 +1,13 @@
+package ar.edu.unq.ttip.sportbook
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SportbookApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
