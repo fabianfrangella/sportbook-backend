f25d42f79c9a48e732b7066b5ee46cce26a253c2
Fabian Frangella <fabianfrangella@gmail.com>
2025-08-30 20:19:38 -0300

    Elimina ruta extra

--- src/main/kotlin/ar/edu/unq/ttip/sportbook/controller/EventController.kt
+++ src/main/kotlin/ar/edu/unq/ttip/sportbook/controller/EventController.kt
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping(value = ["/events", "/event"])
+@RequestMapping(value = ["/event"])
 @CrossOrigin(origins = ["http://localhost:5173"])
 class EventController(val eventService: EventService) {
 
