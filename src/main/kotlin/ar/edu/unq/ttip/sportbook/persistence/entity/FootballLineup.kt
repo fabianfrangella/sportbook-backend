83de3d8a722bb1b738a381b21177f770067549e2
Fabian Frangella <fabianfrangella@gmail.com>
2025-09-13 21:04:50 -0300

    Agrega endpoints para manejo de lineup

--- src/main/kotlin/ar/edu/unq/ttip/sportbook/persistence/entity/FootballLineup.kt
+++ src/main/kotlin/ar/edu/unq/ttip/sportbook/persistence/entity/FootballLineup.kt
@@ -52,29 +52,17 @@ class FootballLineup {
             throw IllegalArgumentException("El jugador ${player.name} ya está en el campo")
         }
         positionsByPlayer.put(position, player)
-        initialLineup.add(player)
+        if (initialLineup.none { it.id == player.id })
+            initialLineup.add(player)
+        if (bench.any { it.id == player.id })
+            bench.removeIf { it.id == player.id }
     }
 
     fun removePlayerFromPosition(position: Position) {
         val player = positionsByPlayer[position] ?: throw IllegalArgumentException("No hay ningún jugador en la posición $position")
         positionsByPlayer.remove(position)
-        initialLineup.remove(player)
+        initialLineup.removeIf{ it.id == player.id}
         bench.add(player)
-    }
-
-    fun substitutePlayer(outPlayer: Player, inPlayer: Player) {
-        if (!initialLineup.contains(outPlayer)) {
-            throw IllegalArgumentException("El jugador ${outPlayer.name} no está en el campo")
-        }
-        if (!bench.contains(inPlayer)) {
-            throw IllegalArgumentException("El jugador ${inPlayer.name} no está en el banco")
-        }
-        val position = positionsByPlayer.filterValues { it == outPlayer }.keys.first()
-        positionsByPlayer.put(position, inPlayer)
-        initialLineup.remove(outPlayer)
-        initialLineup.add(inPlayer)
-        bench.remove(inPlayer)
-        bench.add(outPlayer)
     }
 
     fun addPlayerToBench(player: Player) {
