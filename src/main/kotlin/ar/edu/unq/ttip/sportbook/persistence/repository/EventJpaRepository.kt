6da578b9e8d525554dd6aab66aeab1acbe4c70b1
Fabian Frangella <fabianfrangella@gmail.com>
2025-09-23 23:46:03 -0300

    Agrega endpoint para editar evento

--- src/main/kotlin/ar/edu/unq/ttip/sportbook/persistence/repository/EventJpaRepository.kt
+++ src/main/kotlin/ar/edu/unq/ttip/sportbook/persistence/repository/EventJpaRepository.kt
@@ -3,10 +3,6 @@ package ar.edu.unq.ttip.sportbook.persistence.repository
 import ar.edu.unq.ttip.sportbook.persistence.entity.Event
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
-import org.springframework.data.jpa.repository.Modifying
-import org.springframework.data.jpa.repository.Query
-import org.springframework.data.repository.query.Param
-import java.math.BigDecimal
 
 @Repository
 interface EventJpaRepository : JpaRepository<Event, Long> {}
