8769e6957e0555fab9f73949a27aebc469fff6ea
Fabian Frangella <fabianfrangella@gmail.com>
2025-10-13 15:27:33 -0300

    Agrega funcionalidad de profile picture

---
+++ src/main/kotlin/ar/edu/unq/ttip/sportbook/service/ProfilePictureService.kt
@@ -0,0 +1,30 @@
+package ar.edu.unq.ttip.sportbook.service
+
+import ar.edu.unq.ttip.sportbook.persistence.entity.user.ProfilePicture
+import ar.edu.unq.ttip.sportbook.persistence.entity.user.SportUser
+import ar.edu.unq.ttip.sportbook.persistence.repository.ProfilePictureRepository
+import org.springframework.stereotype.Service
+import org.springframework.web.multipart.MultipartFile
+import jakarta.transaction.Transactional
+
+@Service
+class ProfilePictureService(
+    private val profilePictureRepository: ProfilePictureRepository
+) {
+    @Transactional
+    fun uploadProfilePicture(file: MultipartFile, user: SportUser) {
+        val existingPicture = profilePictureRepository.findByUser(user)
+
+        if (existingPicture != null) {
+            existingPicture.binaryData = file.bytes
+            profilePictureRepository.save(existingPicture)
+        } else {
+            val profilePicture = ProfilePicture(file, user)
+            profilePictureRepository.save(profilePicture)
+        }
+    }
+
+    fun getProfilePicture(user: SportUser): ByteArray? {
+        return profilePictureRepository.findByUser(user)?.binaryData
+    }
+}
